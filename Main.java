e8fb6ac27e54d34c5f5512ff7dac87bb7965d044
AbdirisakGowfe4433 <abdirisakb6@gmail.com>
2025-11-07 13:20:03 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,16 @@
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CharStack<Character> stack1 = new CharStack<>(2);
+        stack1.push('A');
+        stack1.push('B');
+        stack1.push('C');
+        stack1.display();
+
+        System.out.println("===================");
+        Character removedElement = stack1.pop();
+        System.out.println("Element Removed: " + removedElement);
+        System.out.println("Top element now: " + stack1.peek());
+        System.out.println("Current Size: " + stack1.size());
+        System.out.println("Is Stack Empty: " + stack1.isEmpty());
     }
 }
